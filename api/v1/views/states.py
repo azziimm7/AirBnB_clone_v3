08684c0d7701d276a78372e5178f943da9d1e4e7
azziimm7 <azzah31@outlook.com>
2024-05-25 12:50:21 +0000

    put method

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -30,7 +30,7 @@ def get_state_by_id(state_id):
     If the state_id is not linked to any State object, raise a 404 error
     """
 
-    state = storage.get('State', state_id)
+    state = storage.get("State", state_id)
 
     if state is None:
         abort(404)
@@ -47,7 +47,7 @@ def delete_state(state_id):
     If the state_id is not linked to any State object, raise a 404 error
     Returns an empty dictionary with the status code 200
     """
-    state = storage.get('State', state_id)
+    state = storage.get("State", state_id)
     storage.delete(state)
     storage.save()
     return jsonify({})
@@ -75,6 +75,7 @@ def update_state(state_id):
     f the state_id is not linked to any State object, raise a 404 error
     Returns the State object with the status code 200
     """
+    state = storage.get("State", state_id)
     json = request.get_json(silent=True)
     if json is None:
         abort(400, "Not a JSON")
