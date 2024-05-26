6febd49dfe7311cfa3acdd574a7d0faeda7bf753
Amira Walid <amirawaleed012@gmail.com>
2024-05-27 01:02:36 +0300

    update

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -26,7 +26,7 @@ def get_city(state_id):
     for city in state.cities:
         cities_list.append(city.to_dict())
 
-    return jsonify(cities_list), 200
+    return jsonify(cities_list)
 
 
 @app_views.route('/cities/<city_id>', methods=['GET'],
@@ -40,7 +40,7 @@ def get_city_by_id(city_id):
     if city is None:
         abort(404)
 
-    return jsonify(city.to_dict()), 200
+    return jsonify(city.to_dict())
 
 
 @app_views.route('/cities/<city_id>', methods=['DELETE'],
