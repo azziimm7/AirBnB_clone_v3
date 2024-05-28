36de591094fdea1855c4d4ee641adacc7e499a27
azziimm7 <azzah31@outlook.com>
2024-05-28 11:39:45 +0000

    import

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -6,6 +6,8 @@ default RESTFul API actions.
 from api.v1.views import app_views, storage
 from flask import jsonify, abort, request
 from models.place import Place
+from models.state import State
+from models.amenity import Amenity
 from models.city import City
 from models.user import User
 from models import storage_t
