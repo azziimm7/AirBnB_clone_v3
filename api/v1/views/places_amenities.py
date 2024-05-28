4cdd9878efebabef8151d553b02e47ccba46764b
azziimm7 <azzah31@outlook.com>
2024-05-28 07:55:43 +0000

    pycodestyle

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -9,6 +9,7 @@ from flask import jsonify, abort
 from api.v1.views import app_views, storage
 from models.place import Place
 from models.amenity import Amenity
+from os import getenv
 
 
 @app_views.route('/places/<place_id>/amenities', methods=['GET'],
@@ -27,3 +28,33 @@ def get_amenity_place_id(place_id):
         amenity_list.append(obj.to_dict())
 
     return jsonify(amenity_list)
+
+
+@app_views.route('/places/<place_id>/amenities/<amenity_id>',
+                 methods=['DELETE'],
+                 strict_slashes=False)
+def delete_amenity(place_id, amenity_id):
+    """
+    delete amenity linked to place
+    if DBStorage: list, create and delete Amenity objects
+    from amenities relationship
+    FileStorage: list, add and remove Amenity ID in
+    the list amenity_ids of a Place object
+    """
+    place = storage.get(Place, str(place_id))
+    amenity = storage.get(Amenity, str(amenity_id))
+
+    if place or amenity is None:
+        abort(404)
+
+    amenity_ids_list = [amenity.id for amenity in place.amenities]
+
+    if amenity_id not in amenity_ids_list:
+        abort(404)
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        place.amenities.remove(amenity)
+    else:
+        place.amenity_ids.pop(amenity.id)
+    place.save()
+    return jsonify({})
