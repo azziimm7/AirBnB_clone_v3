cf37bd207155bec2a558b40d32674877821867bc
azziimm7 <azzah31@outlook.com>
2024-05-28 11:36:20 +0000

    blan

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -8,6 +8,7 @@ from flask import jsonify, abort, request
 from models.place import Place
 from models.city import City
 from models.user import User
+from models import storage_t
 
 
 @app_views.route('/cities/<city_id>/places', methods=['GET'],
@@ -145,3 +146,50 @@ def update_place(place_id):
 
     place.save()
     return jsonify(place.to_dict()), 200
+
+
+@app_views.route("/places_search", methods=['POST'])
+def search_places():
+    """Search for places based on the request body"""
+    json = request.get_json(silent=True)
+    if json is None:
+        abort(400, "Not a JSON")
+
+    places = [p for p in storage.all(Place).values()]
+    states_ids = json.get('states', None)
+    cities_ids = json.get('cities', None)
+    amenities_ids = json.get('amenities', None)
+
+    if states_ids and len(states_ids) > 0:
+        cities = storage.all(City)
+        state_cities = set([city.id for city in cities.values()
+                            if city.state_id in states_ids])
+    else:
+        state_cities = set()
+
+    if cities_ids and len(cities_ids) > 0:
+        cities_ids = set([c_id for c_id in cities_ids
+                          if storage.get(City, c_id)])
+        state_cities = state_cities.union(cities_ids)
+
+    if len(state_cities) > 0:
+        places = [p for p in places if p.city_id in state_cities]
+
+    result = []
+    if amenities_ids and len(amenities_ids) > 0:
+        amenities_ids = set([a_id for a_id in amenities_ids
+                             if storage.get(Amenity, a_id)])
+
+        for place in places:
+            a_ids = None
+            if storage_t == "db" and place.amenities:
+                a_ids = [a.id for a in place.amenities]
+                del place.amenities
+            elif len(place.amenities) > 0:
+                a_ids = p.amenity_ids
+            if a_ids and all([a_id in a_ids for a_id in amenities_ids]):
+                result.append(place.to_dict())
+    else:
+        result = [p.to_dict() for p in places]
+
+    return jsonify(result)
